8eb433e4082cca3b4d495741152057a70e1c393b
brocode <kelimbth@gmail.com>
2023-12-12 17:46:54 +0300

    minor changes 1.) update Blog model 2.) create User model 3.) import ForeignKey from sqlalchemy

--- db/models.py
+++ db/models.py
@@ -1,4 +1,5 @@
-from sqlalchemy import Column, Integer, String
+from sqlalchemy import Column, Integer, String, ForeignKey
+from sqlalchemy.orm import relationship
 from .database import Base
 
 
@@ -6,6 +7,17 @@ class Blog(Base):
     __tablename__ = 'blogs'
 
     id = Column(Integer, primary_key=True, index=True)
+    user_id = Column(Integer, ForeignKey('users.id'))
+    author = relationship('User', back_populates='blog')
     title = Column(String)
     body = Column(String)
 
+
+class User(Base):
+    __tablename__ = 'users'
+
+    id = Column(Integer, primary_key=True, index=True)
+    name = Column(String)
+    email = Column(String)
+    password = Column(String)
+    blog = relationship('Blog', back_populates='author')
