2871bd629144ed074711807f923ff4c3932c5692
brocode <kelimbth@gmail.com>
2023-12-14 11:19:23 +0300

    update db schema

--- db/schema.py
+++ db/schema.py
@@ -2,10 +2,12 @@ from typing import List
 from pydantic import BaseModel
 
 
-class Blog(BaseModel):
+class BlogBase(BaseModel):
     title: str
     body: str
     
+
+class Blog(BlogBase):
     class Config:
         orm_mode = True
 
